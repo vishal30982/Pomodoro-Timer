0609cf95b0d178eb61075659642db9624003a66f
Utkarsh Sharma <Vishal30982@gmail.com>
2023-11-11 22:49:25 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function showNotification() {
+    if ('Notification' in window) {
+        Notification.requestPermission()
+            .then((permission) => {
+                console.log(permission);
+
+                if (permission === "granted") {
+                    return navigator.serviceWorker.register("notification.js");
+                }
+            })
+            .then((registration) => {
+                if (registration) {
+                    console.log("Service Worker registered with scope:", registration.scope);
+
+                    registration.showNotification("This is a test notification", {
+                        body: "Test note",
+                        icon: "../images/MBC-logo/logo.png"
+                    });
+
+                    registration.active.postMessage('send Notification');
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+}
+
+showNotification();
